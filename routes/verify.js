c26eef5007f5540bdc9c2e3dedf9ef423603b3e2
prateek-shram <prateekja.swal16@gmail.com>
2025-04-05 16:52:55 +0530

    ..

--- routes/verify.js
+++ routes/verify.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mailVerify = require("../controllers/mailController");
 // const sendEmail = require("../controllers/resetPass");
 
+
 router.get("/mail/:uniqueString", mailVerify);
 // router.post("/sendotp",sendEmail);
 module.exports = router;
